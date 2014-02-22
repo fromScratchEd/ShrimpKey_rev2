d63ef25527d8fd3f8721832123f13862e17370e2
sdmeijer <internet@meijer-online.nl>
2014-02-22 12:36:18 +0100

    Comment added in code

--- ShrimpKey/ShrimpKey.ino
+++ ShrimpKey/ShrimpKey.ino
@@ -641,6 +641,7 @@ void sendMouseMovementEvents() {
       #endif
     }
     
+    // release mouse
     if ((mouseHoldTotal == 0) && !(prevMouseHoldTotal == 0)) {
       UsbKeyboard.releaseMouse();
       #ifdef OUTPUTPIN
